c389c130eee7dd6423cb34d8d8bd1c5b54e8710d
Kenta Sekiuchi <86094129+SEKI-2804@users.noreply.github.com>
2023-09-17 18:34:53 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,50 +40,15 @@ $(function () {
 
   //ハンバーガーメニュー
   $(".openbtn").click(function () {//ボタンがクリックされたら
-    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $(this).toggleClass('active-btn');//ボタン自身に activeクラスを付与し
     $("#nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
   });
 
   $("#nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $(".openbtn").removeClass('active-btn');//ボタンの activeクラスを除去し
     $("#nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
   });
-
-  $(function () {
-    $('.hamburger').click(function () {
-      $(this).toggleClass('active');
-
-      if ($(this).hasClass('active')) {
-        $('.globalMenuSp').addClass('active');
-      } else {
-        $('.globalMenuSp').removeClass('active');
-      }
-    });
-  });
-
-  // //fadeup
-  // // 動きのきっかけとなるアニメーションの名前を定義
-  // function fadeAnime() {
-
-  //   $('.fadeUpTrigger').each(function () { //fadeUpTriggerというクラス名が
-  //     var elemPos = $(this).offset().top - 40;//要素より、40px上の
-  //     var scroll = $(window).scrollTop();
-  //     var windowHeight = $(window).height();
-  //     if (scroll >= elemPos - windowHeight) {
-  //       $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
-  //     }
-  //   });
-  // }
-
-  // // 画面をスクロールをしたら動かしたい場合の記述
-  // $(window).scroll(function () {
-  //   fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  // });// ここまで画面をスクロールをしたら動かしたい場合の記述
-
-  // // 画面が読み込まれたらすぐに動かしたい場合の記述
-  // $(window).on('load', function () {
-  //   fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  // });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+  
 });
 
 // ローディング
